12c81ac486b641705e2ff675bad97bab081d5e5c
ScuroGuardiano <scuroguardiano@users.noreply.github.com>
2024-01-07 19:31:15 +0100

    Almost there...

--- include/smart_array_raid_5_reader.hpp
+++ include/smart_array_raid_5_reader.hpp
@@ -3,10 +3,23 @@
 #include <sys/types.h>
 #include <fstream>
 #include <vector>
+#include <optional>
+
+struct SmartArrayRaid5ReaderOptions
+{
+
+    /// @brief stripe size in kilobytes.
+    u_int32_t stripeSize;
+    u_int16_t parityDelay;
+
+    /// @brief drives path
+    std::vector<std::string> drives;
+};
 
 class SmartArrayRaid5Reader
 {
 public:
+    SmartArrayRaid5Reader(SmartArrayRaid5ReaderOptions& options);
     int read(void *buf, u_int32_t len, u_int64_t offset);
 
 private:
@@ -15,7 +28,7 @@ private:
 
     // Smallest drive in the array
     u_int64_t singleDriveSize;
-    std::vector<std::ifstream> drives;
+    std::vector<std::optional<std::ifstream>> drives;
 
     u_int64_t totalArraySize();
     // Stripes will be index from 0.
@@ -24,4 +37,6 @@ private:
     u_int32_t stripeRelativeOffset(u_int64_t stripenumber, u_int64_t offset);
     u_int16_t stripeDriveNumber(u_int64_t stripenumber);
     u_int64_t stripeDriveOffset(u_int64_t stripenum, u_int32_t stripeRelativeOffset);
+    u_int32_t readFromStripe(void* buf, u_int64_t stripenum, u_int32_t stripeRelativeOffset, u_int32_t len);
+    u_int32_t recoverForDrive(void* buf, u_int16_t drivenum, u_int64_t driveOffset, u_int32_t len);
 };
--- src/smart_array_raid_5_reader.cpp
+++ src/smart_array_raid_5_reader.cpp
@@ -1,5 +1,43 @@
 #include "smart_array_raid_5_reader.hpp"
 #include <math.h>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+SmartArrayRaid5Reader::SmartArrayRaid5Reader(SmartArrayRaid5ReaderOptions &options)
+{
+    this->parityDelay = parityDelay;
+    this->stripeSizeInBytes = options.stripeSize * 1024;
+    
+    int missingDrives = 0;
+    for (auto drive : options.drives)
+    {
+        if (drive.empty())
+        {
+            throw std::runtime_error("read with missing drive is currently not implemeted.");
+            if (missingDrives > 0)
+            {
+                throw std::invalid_argument("For RAID 5 only 1 missing drive is allowed.");
+            }
+            missingDrives++;
+            this->drives.push_back(std::nullopt);
+        }
+
+        std::ifstream driveHandle(drive, std::ios::binary);
+
+        if (!driveHandle.is_open())
+        {
+            std::stringstream errMsgStream;
+            errMsgStream << "Could not open drive "
+                << drive << " Reason: "
+                << errno;
+
+            throw std::runtime_error(errMsgStream.str());
+        }
+
+        this->drives.push_back(std::optional(std::move(driveHandle)));
+    }
+}
 
 int SmartArrayRaid5Reader::read(void *buf, u_int32_t len, u_int64_t offset)
 {
@@ -46,4 +84,51 @@ u_int64_t SmartArrayRaid5Reader::stripeDriveOffset(u_int64_t stripenum, u_int32_
     u_int64_t currentStripeRow = stripenum / (drives.size() - 1);
 
     return (currentStripeRow * this->stripeSizeInBytes) + stripeRelativeOffset;
+}
+
+u_int32_t SmartArrayRaid5Reader::readFromStripe(void *buf, u_int64_t stripenum, u_int32_t stripeRelativeOffset, u_int32_t len)
+{
+    auto drivenum = stripeDriveNumber(stripenum);
+    auto driveOffset = stripeDriveOffset(stripenum, stripeRelativeOffset);
+    
+    if ((len + stripeRelativeOffset) > this->stripeSizeInBytes)
+    {
+        std::stringstream errMsg;
+        errMsg << "Exceeded stripe data area, "
+            << "stripeRelativeOffset = " << stripeRelativeOffset << ";"
+            << "len = " << len;
+        throw std::out_of_range(errMsg.str());
+    }
+
+    auto& driveHandleOpt = this->drives[drivenum];
+
+    if (!driveHandleOpt.has_value()) 
+    {
+        return this->recoverForDrive(buf, drivenum, driveOffset, len);
+    }
+
+    auto& driveHandle = driveHandleOpt.value();
+    driveHandle.read((char*)buf, len);
+    
+    if (driveHandle.fail())
+    {
+        std::stringstream errMsg;
+        errMsg << "Reading from drive has failed. ";
+        if (driveHandle.eof()) {
+            errMsg << "Reason: end of file.";
+        }
+        else {
+            errMsg << "Reason: unknown, errno: " << errno;
+        }
+
+        throw std::out_of_range(errMsg.str());
+    }
+
+    return 0;
+}
+
+u_int32_t SmartArrayRaid5Reader::recoverForDrive(void *buf, u_int16_t drivenum, u_int64_t driveOffset, u_int32_t len)
+{
+    throw std::runtime_error("Recovery from parity is not implemented yet");
+    return u_int32_t();
 }
